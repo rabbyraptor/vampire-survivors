788433fc29076de14ab3241d7a0411b00c8e1fff
Mathias Hammer <rabbyraptor@hotmail.com>
2023-02-18 16:19:42 +0100

    Create objPlayer.vue

---
+++ src/components/gameObjects/objPlayer.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="player" ref="player" :style="playerStyles" tabindex="0"></div>
+</template>
+
+<script>
+import { mapGetters, mapMutations } from "vuex";
+
+export default {
+  mounted() {
+    // Set the initial position of the player element on the page
+    // this.$refs.player.style.transform = `translate(${this.x}px, ${this.y}px)`;
+    const fps = 1 / 60;
+    setInterval(this.updatePlayerPosition, fps); //
+
+    // Set up a listener for the 'keydown' event to detect when the player moves
+    window.addEventListener("keydown", this.handleKeyDown);
+    window.addEventListener("keyup", this.handleKeyUp);
+  },
+  updated() {
+    // Update the background position of the player element to show the next frame of the animation
+    this.$refs.player.style.backgroundPosition = `${
+      -this.frame * this.player.dimensions.width
+    }px 0`;
+  },
+  data() {
+    return {
+      playerX: 0, // The x-coordinate of the player's position
+      playerY: 0, // The y-coordinate of the player's position
+      frame: 0, // The current frame of the animation
+      intervalId: null, // The ID of the interval that updates the animation
+      playerSpeed: 6,
+      keysPressed: {},
+      playerWalkDirection: "right",
+    };
+  },
+  computed: {
+    ...mapGetters({
+      player: "objPlayer/getPlayer",
+    }),
+    playerStyles() {
+      return `
+      left: ${this.playerX}px; 
+      top: ${this.playerY}px; 
+      width: ${this.player.dimensions.width}px; 
+      height: ${this.player.dimensions.height}px;
+      transform: translate(-50%, -50%) scale(${
+        this.playerWalkDirection === "left" ? -1 : 1
+      }, 1);
+      `;
+    },
+  },
+  watch: {
+    playerY() {
+      this.setPlayerPosition({ x: this.playerX, y: this.playerY });
+    },
+    playerX() {
+      this.setPlayerPosition({ x: this.playerX, y: this.playerY });
+    },
+  },
+  methods: {
+    ...mapMutations({
+      setPlayerPosition: "objPlayer/setPosition",
+    }),
+    handleKeyUp(event) {
+      this.keysPressed[event.key] = false;
+    },
+    handleKeyDown(event) {
+      this.keysPressed[event.key] = true;
+    },
+    updatePlayerPosition() {
+      // Move the player when the user presses the arrow keys
+      if (this.keysPressed["w"]) {
+        this.playerY -= this.playerSpeed;
+        this.startAnimation();
+      }
+      if (this.keysPressed["a"]) {
+        this.playerX -= this.playerSpeed;
+        this.playerWalkDirection = "left";
+        this.startAnimation();
+      }
+      if (this.keysPressed["s"]) {
+        this.playerY += this.playerSpeed;
+        this.startAnimation();
+      }
+      if (this.keysPressed["d"]) {
+        this.playerX += this.playerSpeed;
+        this.playerWalkDirection = "right";
+        this.startAnimation();
+      } else if (
+        !this.keysPressed["w"] &&
+        !this.keysPressed["a"] &&
+        !this.keysPressed["s"] &&
+        !this.keysPressed["s"]
+      ) {
+        this.stopAnimation();
+      }
+    },
+    startAnimation() {
+      if (this.intervalId) {
+        return;
+      }
+      this.intervalId = setInterval(() => {
+        this.frame = (this.frame + 1) % 5; // There are 5 frames in the animation
+      }, 120);
+    },
+    stopAnimation() {
+      // Stop the animation by clearing the interval and resetting the frame to the first frame of the animation
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+      //this.frame = 0;
+    },
+  },
+  beforeDestroy() {
+    // Stop the animation and remove the event listener when the player is destroyed
+    this.stopAnimation();
+    window.removeEventListener("keydown", this.handleKeyDown);
+  },
+};
+</script>
+
+<style scoped>
+.player {
+  position: absolute;
+  /* transform: translate(-50%, -50%); */
+  background-color: transparent;
+  background-image: url("~@/assets/sprites/man_walking/man_walking_trans.png");
+  background-size: calc(740px * 0.5) calc(199px * 0.5); /* The sprite is 6 frames wide */
+}
+</style>

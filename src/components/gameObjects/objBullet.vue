dd16a8a0a37ad7753c45075a6376e47f8bc5a5b9
Mathias Hammer <rabbyraptor@hotmail.com>
2023-02-18 16:19:46 +0100

    Create objBullet.vue

---
+++ src/components/gameObjects/objBullet.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bullet" :style="{ left: x + 'px', top: y + 'px' }">83</div>
+</template>
+
+<script>
+export default {
+  props: {
+    x: {
+      type: Number,
+      default: null,
+    },
+    y: {
+      type: Number,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style>
+.bullet {
+  position: absolute;
+  transform: translate(-50%, -50%);
+}
+</style>

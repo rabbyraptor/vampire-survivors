ed2748b4ddd8ccfa7fe62cf1a4cfb37ccbbfb432
Mathias Hammer <rabbyraptor@hotmail.com>
2023-02-18 16:17:07 +0100

    Init install

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <GameView />
+  </div>
+</template>
+
+<script>
+import GameView from "./components/GameView.vue";
+
+export default {
+  name: "App",
+  components: {
+    GameView,
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
